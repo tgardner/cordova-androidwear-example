2649d2e1332888e3eebbd129c95ae683ee8de9dc
Trent Gardner <trent.gardner@gmail.com>
2018-01-06 23:13:15 +1030

    Updated for Java 1.6 compatibility

--- wear/app/src/main/java/io/cordova/hellocordova/MainActivity.java
+++ wear/app/src/main/java/io/cordova/hellocordova/MainActivity.java
@@ -1,7 +1,6 @@
 package io.cordova.hellocordova;
 
 import android.app.Activity;
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,8 +9,8 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.wearable.CapabilityClient;
 import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.MessageClient;
@@ -20,95 +19,93 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends Activity implements
-        MessageClient.OnMessageReceivedListener,
-        CapabilityClient.OnCapabilityChangedListener {
+		MessageClient.OnMessageReceivedListener,
+		CapabilityClient.OnCapabilityChangedListener {
 
-    public static final String MESSAGE_PATH = "/NewMessage";
+	public static final String MESSAGE_PATH = "/NewMessage";
 
-    private TextView mTextView;
+	private TextView mTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        mTextView = findViewById(R.id.text);
-        Button mButton = findViewById(R.id.button);
+		mTextView = findViewById(R.id.text);
+		Button mButton = findViewById(R.id.button);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                (new Thread(new MessageRunnable())).start();
-                logMessage("Message sent to Cordova");
-            }
-        });
-    }
+		mButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				(new Thread(new MessageRunnable())).start();
+			}
+		});
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-        // Instantiates clients without member variables, as clients are inexpensive to create and
-        // won't lose their listeners. (They are cached and shared between GoogleApi instances.)
-        Wearable.getMessageClient(this).addListener(this);
-        Wearable.getCapabilityClient(this)
-                .addListener(
-                        this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE);
-    }
+		// Instantiates clients without member variables, as clients are inexpensive to create and
+		// won't lose their listeners. (They are cached and shared between GoogleApi instances.)
+		Wearable.getMessageClient(this).addListener(this);
+		Wearable.getCapabilityClient(this)
+				.addListener(
+						this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE);
+	}
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+	@Override
+	protected void onPause() {
+		super.onPause();
 
-        Wearable.getMessageClient(this).removeListener(this);
-        Wearable.getCapabilityClient(this).removeListener(this);
-    }
+		Wearable.getMessageClient(this).removeListener(this);
+		Wearable.getCapabilityClient(this).removeListener(this);
+	}
 
-    private void logMessage(final String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mTextView.append(message);
-                mTextView.append("\n");
-            }
-        });
-    }
+	private void logMessage(final String message) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				mTextView.append(message);
+				mTextView.append("\n");
+			}
+		});
+	}
 
-    @Override
-    public void onMessageReceived(@NonNull MessageEvent messageEvent) {
-        if(messageEvent.getPath().equals(MESSAGE_PATH)) {
-            logMessage(new String(messageEvent.getData()));
-        }
-    }
+	@Override
+	public void onMessageReceived(@NonNull MessageEvent messageEvent) {
+		if (messageEvent.getPath().equals(MESSAGE_PATH)) {
+			logMessage(new String(messageEvent.getData()));
+		}
+	}
 
-    @Override
-    public void onCapabilityChanged(@NonNull CapabilityInfo capabilityInfo) {
-        logMessage("onCapabilityChanged: " + capabilityInfo.getName());
-    }
+	@Override
+	public void onCapabilityChanged(@NonNull CapabilityInfo capabilityInfo) {
+		logMessage("onCapabilityChanged: " + capabilityInfo.getName());
+	}
 
-    private class MessageRunnable implements Runnable {
+	private class MessageRunnable implements Runnable {
 
-        @Override
-        public void run() {
-            try {
-                Context mContext = getApplicationContext();
-                List<Node> nodes =
-                        Tasks.await(Wearable.getNodeClient(mContext)
-                                .getConnectedNodes());
+		@Override
+		public void run() {
+			Task<List<Node>> nodesTask = Wearable.getNodeClient(MainActivity.this)
+					.getConnectedNodes();
+			nodesTask.addOnSuccessListener(new OnSuccessListener<List<Node>>() {
 
-                for (Node node : nodes) {
-                    Task<Integer> messageTask = Wearable.getMessageClient(mContext).sendMessage(node.getId(),
-                            MESSAGE_PATH, "Hello from AndroidWear".getBytes());
-                    Tasks.await(messageTask);
-                }
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+				@Override
+				public void onSuccess(List<Node> nodes) {
+					for (Node node : nodes) {
+						Wearable.getMessageClient(MainActivity.this)
+								.sendMessage(node.getId(), MESSAGE_PATH, "Hello from AndroidWear".getBytes());
+					}
+
+					logMessage("Message sent to Cordova");
+				}
+			});
+		}
+	}
 }
